248e3e156b97d8f80e09b64c8225d9a4b90e4db0
Roman Grout <roman.grout@epitech.eu>
2015-03-03 13:58:27 +0100

    bug mineur

--- map.c
+++ map.c
@@ -9,3 +9,14 @@ char		get_case(int x, int y, t_map *map)
       tmp = shmat(id, NULL, SHM_R | SHM_W);
       return (tmp[x]);
 }
+
+void		dump_map(t_map *map)
+{
+  int		i;
+  
+  i = 1;
+  while (i != LIN_NBR + 1)
+    {
+      map = (void*)map;
+    }
+}
