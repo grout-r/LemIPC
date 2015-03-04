00fef191607d7ff40ea730f0072cb65ecfd125c7
Roman Grout <roman.grout@epitech.eu>
2015-03-04 16:30:27 +0100

    scanning

--- map.c
+++ map.c
@@ -35,18 +35,26 @@ t_pos		check_around(t_pos *pos, t_map *map, t_ia *ia)
       tmp->y = pos->y;
       while (tmp->x != pos->x + (segment - 1))
 	{
+	  if (get_case(tmp, map) != 0 && get_case(tmp, map) != ia->team)
+	    return (tmp);
 	  tmp->x++;
 	}
       while (tmp->y != pos-> + (segment - 1))
 	{
+	  if (get_case(tmp, map) != 0 && get_case(tmp, map) != ia->team)
+	    return (tmp);
 	  tmp->y++;
 	}
       while (tmp->x != pos->x)
 	{
+	  if (get_case(tmp, map) != 0 && get_case(tmp, map) != ia->team)
+	    return (tmp);
 	  tmp->x--;
 	}
       while (tmp->y != pos-y)
 	{
+	  if (get_case(tmp, map) != 0 && get_case(tmp, map) != ia->team)
+	    return (tmp);
 	  tmp->y--;
 	}
       segment += 2;
